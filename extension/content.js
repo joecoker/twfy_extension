a0aaf26a981a0ad39bafc174fcbef61e4386de6a
Joe Coker <joeseph.coker@digital.homeoffice.gov.uk>
2019-07-01 21:02:10 +0100

    Increased speed

--- extension/content.js
+++ extension/content.js
@@ -1,5 +1,9 @@
 'use strict'
+const AhoCorasick = require('ahocorasick');
 const mpJSON = require('./mp.json');
+const mpArray = require('./mpArray.json')
+
+let startTime = performance.now();
 
 function sendToBackground(id) {
   chrome.runtime.sendMessage(id);
@@ -11,7 +15,6 @@ function addClickEvent(classArray) {
 
     Array.from(classList).forEach(element => {
       element.addEventListener('click', function(event) {
-        console.log(event);
         event.preventDefault()
         event.stopPropagation();
         sendToBackground(className);
@@ -20,24 +23,59 @@ function addClickEvent(classArray) {
   });
 }
 
-function highlightText(mpJSON) {
-  let classArray = [];
+// function highlightText(mpJSON) {
+//   let classArray = [];
   
-  mpJSON.forEach(mpObj => {
-    let name = mpObj.mpFullName;
-    let className = mpObj.mpId;
+//   mpJSON.forEach(mpObj => {
+//     let name = mpObj.mpFullName;
+//     let className = mpObj.mpId;
+//     let nameRegex = new RegExp(name, 'g');
+
+//     if(document.body.innerHTML.includes(name)) {
+//       let profileLink = "<span href=# class=" + className + " style='color: #62B356'>" + name + "</span>";
+
+//       document.body.innerHTML = document.body.innerHTML.replace(nameRegex, profileLink);
+//       classArray.push(className)
+//     };
+//   });
+
+//   addClickEvent(classArray)
+// }
+
+function highlightTextAC(mpArray) {
+  let ac = new AhoCorasick(mpArray)
+
+  let results = ac.search(document.body.innerText)
+
+  let mpList = results.flat(2).filter((element) => {
+    return (typeof element == 'string');
+  })
+
+  let uniqueMPList = [...new Set(mpList)];
+
+  let classArray = []
+
+  // Need to see which websites it can run and cant run on
+
+  // Clean up
+
+  for(let i = 0; i < uniqueMPList.length; i++) {
+    let name = uniqueMPList[i]
+    let className = name.toLowerCase().replace(/ /g,"_");
     let nameRegex = new RegExp(name, 'g');
 
-    if(document.body.innerHTML.includes(name)) {
-      let profileLink = "<span href=# class=" + className + " style='color: #62B356'>" + name + "</span>";
-
-      document.body.innerHTML = document.body.innerHTML.replace(nameRegex, profileLink);
-      classArray.push(className)
-    };
-  });
+    let profileLink = "<span href=# class=" + className + " style='color: #62B356'>" + name + "</span>";
+    document.body.innerHTML = document.body.innerHTML.replace(nameRegex, profileLink);
+    classArray.push(className)
+  }
 
   addClickEvent(classArray)
 }
 
-highlightText(mpJSON);
+// highlightText(mpJSON);
 
+highlightTextAC(mpArray);
+
+let finishTime = performance.now();
+
+console.log("Content script took " + (finishTime - startTime) + " milliseconds to run")
